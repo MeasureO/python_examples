7e6d3de3b7fad7da594d23cbc4f6717c5274e821
MeasureO <42300157+MeasureO@users.noreply.github.com>
2022-08-06 03:17:14 +0300

    Create decorator5.py

---
+++ decorator5.py
@@ -0,0 +1,18 @@
+def dedefis(func):
+    def wrapper(*args):
+        s = func(*args)
+        while '--' in s:
+            s = s.replace('--', '-')
+        return s
+
+    return wrapper
+
+@dedefis   
+def kirilitsa(s):
+    t = {'ё': 'yo', 'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ж': 'zh',
+     'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p',
+     'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh',
+     'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
+    return ''.join(['-' if j in ": ;.,_" else j for j in [t[i] if i in t else i for i in list(s.lower())]])
+
+print(kirilitsa(input()))
