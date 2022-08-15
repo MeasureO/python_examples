dcf3387dc739cea3d7d9beaff2168e39d9554c03
MeasureO <42300157+MeasureO@users.noreply.github.com>
2022-08-15 18:01:57 +0300

    Create Graph.py

---
+++ Graph.py
@@ -0,0 +1,29 @@
+class Graph:
+    def __init__(self, data):
+        self.data = data.copy()
+        self.is_show = True
+    def set_data(self, data):
+        self.data = data.copy()
+    def show_table(self):
+        if self.is_show:
+            print(*self.data)
+        else:
+            print("Отображение данных закрыто")
+    def show_graph(self):
+        if self.is_show:
+            print(f"Графическое отображение данных:", *self.data)
+        else:
+            print("Отображение данных закрыто")
+    def show_bar(self):
+        if self.is_show:
+            print(f"Столбчатая диаграмма:", *self.data )
+        else:
+            print("Отображение данных закрыто")
+    def set_show(self, fl_show):
+        self.is_show = fl_show
+        
+data_graph = list(map(int, input().split()))
+gr = Graph(data_graph)
+gr.show_bar()
+gr.set_show(fl_show=False)
+gr.show_table()
